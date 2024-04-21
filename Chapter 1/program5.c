5e88013e8e5aadf42f87679e35811d3eb156d38e
laxmination <laxmikathayat52@gmail.com>
2024-04-21 23:48:11 +0545

    Program to calculate area of circle

---
+++ Chapter 1/program5.c
@@ -0,0 +1,11 @@
+// Program to calculate area of the circle
+#include<stdio.h>
+
+int main()
+{
+    float radius;
+    printf("Enter radius: ");
+    scanf("%f", &radius);
+    printf("Area of the circle is %f",3.14 * radius * radius);
+    return 0;
+}
