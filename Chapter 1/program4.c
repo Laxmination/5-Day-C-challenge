378e06166ff60ebb97f199a748e787beede9aa2c
laxmination <laxmikathayat52@gmail.com>
2024-04-21 23:24:53 +0545

    program to calculate area of square

---
+++ Chapter 1/program4.c
@@ -0,0 +1,13 @@
+// Program to find area of square
+
+#include<stdio.h>
+
+int main()
+{
+    int side;
+     printf("Enter the value of side: ");
+     scanf("%d", &side);
+
+     printf("The area of square is: %d ", side *side);
+     return 0;
+}
