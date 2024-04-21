64c03725e6252b6c8c0061cf1d32bd48e19ff299
laxmination <laxmikathayat52@gmail.com>
2024-04-21 23:17:22 +0545

    sum of two numbers

---
+++ Chapter 1/program3.c
@@ -0,0 +1,16 @@
+// Sum of 2 numbers
+#include<stdio.h>
+
+int main()
+{
+    int a, b;
+
+    printf("Enter a\n");
+    scanf("%d", &a);
+    printf("Enter b\n");
+    scanf("%d", &b);
+
+    printf("Sum of a and b is: %d\n", a +b);
+
+    return 0;
+}
