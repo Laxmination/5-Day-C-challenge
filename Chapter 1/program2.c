6463fca84a0cc096f7f8c5df6fe1ecc838a92f03
laxmination <laxmikathayat52@gmail.com>
2024-04-21 22:52:41 +0545

    A simple example

---
+++ Chapter 1/program2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int age = 22;  // int is data type and age is variable here
+    float pi = 3.14;
+    char percentage = '%';
+
+
+    printf("Age is %d\n", age); //this function will give the output of varable age.
+    printf("Value of pi is %f\n", pi);
+    printf("Percentage is %c\n", percentage);
+
+    return 0;
+}
+
+// This is single line comment 
+/*This 
+is multi-line
+comment*/
