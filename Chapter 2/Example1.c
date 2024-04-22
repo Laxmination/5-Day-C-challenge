783b754f5a5c79b36d9e252ac37043a466755fc3
laxmination <laxmikathayat52@gmail.com>
2024-04-22 07:55:44 +0545

    Assignment operator

--- Chapter 2/Example1.c
+++ Chapter 2/Example1.c
@@ -30,5 +30,19 @@ printf("%d \n", 3<4 && 3<5);
 printf("%d \n", !(3<4 && 3<5));
 printf("%d \n", !(4<3 || 5<3));
 
+//Assignment operator
+int a = 10;
+a += 10;
+printf("a+10 = %d \n", a);
+a -= 10;
+printf("a-10 = %d \n", a);
+a *= 10;
+printf("a*10 = %d \n", a);
+a /= 10;
+printf("a/10 = %d \n", a);
+a %= 10;
+printf("a%c10 = %d \n", '%', a);
+
+
 return 0;
 }
