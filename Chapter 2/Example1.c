45621a7d5c596d45b2aee11b63e392a35812d985
laxmination <laxmikathayat52@gmail.com>
2024-04-22 07:49:16 +0545

    Relational operator

--- Chapter 2/Example1.c
+++ Chapter 2/Example1.c
@@ -6,4 +6,17 @@ int newPrice = oldPrice + 10;
 printf("new Price is : %d", newPrice);
 int rupee = 1, dollar;
 dollar = 74;
+
+printf("%d \n", 4==4);
+printf("%d \n", 4<3);
+printf("%d \n", 3<4);
+printf("%d \n", 4<4);
+printf("%d \n", 4<=4);
+printf("%d \n", 4>3);
+printf("%d \n", 3>4);
+printf("%d \n", 4>4);
+printf("%d \n", 4>=4);
+printf("%d \n", 4!=4);
+printf("%d \n", 3!=4);
+return 0;
 }
