3a35bba4e7bcaf1dba76d65ce3c05e76a9f5600f
laxmination <laxmikathayat52@gmail.com>
2024-04-22 07:46:34 +0545

    type declaration instructions

--- Chapter 2/Example.c
+++ Chapter 2/Example.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main() {
 int a = 1, b = 2, c = 3;
-//valid
+
 a = b + c;
 
 printf("%d \n", 3 % 2);
---
+++ Chapter 2/Example1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+int price = 25;
+int oldPrice = price;
+int newPrice = oldPrice + 10;
+printf("new Price is : %d", newPrice);
+int rupee = 1, dollar;
+dollar = 74;
+}
