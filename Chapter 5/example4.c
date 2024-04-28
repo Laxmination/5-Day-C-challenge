4d056444d038dec9ea90c9906c6a1f28ab6a51b7
laxmination <laxmikathayat52@gmail.com>
2024-04-29 00:26:05 +0545

    Factorial calculator using recursion

---
+++ Chapter 5/example4.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int factorial(int n);
+
+int main()
+{
+    int n;
+    printf("Enter a number :");
+    scanf("%d", &n);
+    printf("Factorial is : %d", factorial(n));
+    return 0;
+}
+
+int factorial(int n)
+{
+    if(n == 0)
+    {
+        return 1;
+    }
+    else if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        int facn1 = factorial(n -1);
+        int fac = facn1 * n;
+        return fac;
+    }
+    
+}
