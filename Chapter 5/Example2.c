5fa4f4d0d4f85c92cbfe8540900fce4743e49b38
laxmination <laxmikathayat52@gmail.com>
2024-04-28 19:22:18 +0545

    A simple example of userdefined function

---
+++ Chapter 5/Example2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void printhello(); //function Prototype/ Declaration
+
+int main()
+{
+    printhello(); //Function call
+    return 0;
+}
+
+void printhello()  //Function Definition
+{
+    printf("Hello, Friends!\n");
+}
