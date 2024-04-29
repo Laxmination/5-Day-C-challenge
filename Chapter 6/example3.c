f946ff56422024880aa81902714f62b36dccd08c
laxmination <laxmikathayat52@gmail.com>
2024-04-29 09:07:27 +0545

    Program to swap two numbers

---
+++ Chapter 6/example3.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+void swap(int a, int b);
+void _swap(int *a, int *b);
+
+int main()
+{
+    int x = 3, y = 5;
+
+    swap(x, y);
+    printf("x =%d and y = %d\n", x, y);
+
+    _swap(&x, &y);
+    printf("x =%d and y =%d\n", x, y);
+
+    return 0;
+}
+
+void swap(int a, int b)
+{
+    int t = a;
+    a = b;
+    b = a;
+    a = t;
+}
+
+void _swap(int *a, int *b)
+{
+    int t = *a;
+    *a = *b;
+    *b = *a;
+    *a = t;
+}
